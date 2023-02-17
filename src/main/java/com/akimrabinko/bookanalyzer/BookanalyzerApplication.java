273129079306acf31a9f262478be055e7c7587df
akimrabinko <nicroman5@gmail.com>
2023-02-17 14:48:22 +0200

    Initial commit

---
+++ src/main/java/com/akimrabinko/bookanalyzer/BookanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.akimrabinko.bookanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akimrabinko/bookanalyzer/BookanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akimrabinko.bookanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
