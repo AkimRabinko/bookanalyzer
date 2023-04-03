0000e257a6ad606d5c01c4cf5e4c95888bc47d77
akimrabinko <nicroman5@gmail.com>
2023-04-03 18:35:18 +0300

    Updated book analysis view.

--- frontend/views/books-analysis.ts
+++ frontend/views/books-analysis.ts
@@ -8,17 +8,33 @@ import {applyTheme} from 'Frontend/generated/theme';
 import '@vaadin/grid';
 import '@vaadin/grid/vaadin-grid-filter-column.js';
 import '@vaadin/grid/vaadin-grid-sort-column.js';
-import {GridActiveItemChangedEvent} from "@vaadin/grid";
-import {gridRowDetailsRenderer} from "@vaadin/grid/lit";
+import {columnBodyRenderer} from "@vaadin/grid/lit";
+import {DirectiveResult} from "lit/directive";
+import {dialogRenderer, DialogRendererDirective} from "@vaadin/dialog/lit";
+import {DialogOpenedChangedEvent} from "@vaadin/dialog";
 
 @customElement("books-analysis-template")
 class BooksAnalysis extends LitElement {
+    private wordsUsagesDialogRenderer: DirectiveResult<typeof DialogRendererDirective> = dialogRenderer(
+        () => html`
+            <vaadin-grid .items="${this.detailsOpenedItem.wordsUsages}">
+                <vaadin-grid-sort-column frozen path="word"></vaadin-grid-sort-column>
+                <vaadin-grid-sort-column frozen
+                                         path="occurrences"></vaadin-grid-sort-column>
+            </vaadin-grid>`);
+    private wordsUsagesDialogOpened = false;
 
     @state()
     protected booksAnalysis: BookAnalysisDto[] = [];
 
     @state()
-    protected detailsOpenedItem: BookAnalysisDto[] = []
+    protected detailsOpenedItem: BookAnalysisDto = {
+        bookAuthor: '',
+        bookName: '',
+        wordsUsages: [],
+        id: 0,
+        wordsCount: 0
+    };
 
     protected override createRenderRoot() {
         const root = super.createRenderRoot();
@@ -28,7 +44,6 @@ class BooksAnalysis extends LitElement {
 
     protected override async firstUpdated() {
         this.booksAnalysis = await this.getBookAnalysis()
-        console.log('done');
     }
 
     private async getBookAnalysis(): Promise<BookAnalysisDto[]> {
@@ -39,27 +54,36 @@ class BooksAnalysis extends LitElement {
         return html`
             <vaadin-grid .items="${this.booksAnalysis}"
                          id="book-grid"
-                         theme="row-stripes no-border"
-                         .detailsOpenedItems="${this.detailsOpenedItem}"
-                         @active-item-changed="${(event: GridActiveItemChangedEvent<BookAnalysisDto>) => {
-                             const bookAnalysis = event.detail.value;
-                             this.detailsOpenedItem = bookAnalysis ? [bookAnalysis] : [];
-                         }}"
-                         ${gridRowDetailsRenderer<BookAnalysisDto>(
-                                 (bookAnalysis) => html`
-                                     <vaadin-vertical-layout>
-                                         <vaadin-grid .items="${bookAnalysis.wordsUsages}">
-                                             <vaadin-grid-sort-column frozen path="word"></vaadin-grid-sort-column>
-                                             <vaadin-grid-sort-column frozen
-                                                                      path="occurrences"></vaadin-grid-sort-column>
-                                         </vaadin-grid>
-                                     </vaadin-vertical-layout>`)}>
+                         theme="row-stripes no-border">
                 <vaadin-grid-filter-column frozen path="id"></vaadin-grid-filter-column>
                 <vaadin-grid-filter-column frozen path="bookName"></vaadin-grid-filter-column>
                 <vaadin-grid-filter-column frozen path="bookAuthor"></vaadin-grid-filter-column>
                 <vaadin-grid-filter-column frozen path="launchStart"></vaadin-grid-filter-column>
                 <vaadin-grid-filter-column frozen path="launchEnd"></vaadin-grid-filter-column>
                 <vaadin-grid-filter-column frozen path="wordsCount"></vaadin-grid-filter-column>
+                <vaadin-grid-column frozen ${columnBodyRenderer<BookAnalysisDto>(
+                        (book) => html`
+                            <vaadin-button
+                                    theme="tertiary"
+                                    @click="${() => {
+                                        this.detailsOpenedItem = book;
+                                        this.wordsUsagesDialogOpened = true;
+                                    }}">
+                                View words Usages
+                            </vaadin-button>`,
+                        []
+                )}></vaadin-grid-column>
+
+                <vaadin-dialog
+                        id="book-save-dialog"
+                        header-title="${this.detailsOpenedItem.bookName} / ${this.detailsOpenedItem.bookAuthor}"
+                        draggable
+                        .opened="${this.wordsUsagesDialogOpened}"
+                        @opened-changed="${(event: DialogOpenedChangedEvent) => {
+                            this.wordsUsagesDialogOpened = event.detail.value;
+                        }}"
+                        ${this.wordsUsagesDialogRenderer}>
+                </vaadin-dialog>
             </vaadin-grid>
         `;
     }
